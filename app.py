31549cd6084a566f73bd2e29d9327ecbda1faf3b
Sanay Nesargi <sanaynesargi@gmail.com>
2023-08-07 13:43:38 -0500

    Fixed another debug issue

--- app.py
+++ app.py
@@ -544,7 +544,7 @@ def get_profile_list():
             entry = {
                 "profile_type": {
                     "title": "Profile Type",
-                    "value": "Rental"
+                    "value": "Fix and Flip"
                 },
                 "profile_name": {
                     "title": "Profile Name",
